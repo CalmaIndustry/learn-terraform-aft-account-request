55142a7d9c17e8f2167a91421838a08d294505cf
Calma Industry <51451578+CalmaIndustry@users.noreply.github.com>
2024-02-14 17:03:55 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "testuser" {
 
   account_customizations_name = "sandboxtestcalma"
 }
+
+module "ticalmalast" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "ticalmalast@gmail.com"
+    AccountName               = "ticalmalast"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "ticalmalast@gmail.com"
+    SSOUserFirstName          = "ticalmalast1"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandboxtestcalma"
+}
